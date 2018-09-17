96d78f036a92ace67cb22a07346f0d5b01e74749
zilehda <zilehda.tariq@nexdegree.com>
2018-09-17 14:14:25 +0500

    display book completed

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,18 +6,33 @@ const getBookQuery = gql`
 {
     books{
         name
+        genre
         id
     }
 }
 `
 
 class BookList extends Component {
+
+  DisplayBooks(){
+      var data = this.props.data
+      if(data.loading)
+      {
+        return (<div>data is loading...</div>)
+      }
+      else
+      {
+          return data.books.map(book =>{
+              return (<li key={book.id}>{book.name} {book.genre}</li>)
+          })
+      } 
+  }
   render() {
-      console.log(this);
+      console.log(this.props.data);
     return (
       <div>
       <ul  id="book-list">
-        <li>book 1</li>
+        {this.DisplayBooks()}
       </ul>
       </div>
     );
